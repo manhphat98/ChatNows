3ea4f46860221b08c94551e007056fa96ab014c6
manhphat9822 <2024802010266@student.tdmu.edu.vn>
2024-05-23 16:16:56 +0700

    first commit

---
+++ screens/Login.js
@@ -0,0 +1,148 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Image, SafeAreaView, TouchableOpacity, StatusBar, Alert } from "react-native";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config/firebase";
+import { Ionicons } from '@expo/vector-icons'; // Bạn cần phải cài đặt @expo/vector-icons
+const backImage = require("../assets/backImage.png");
+const iconImage = require("../assets/logo.png");
+
+export default function Login({ navigation }) {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const onHandleLogin = () => {
+    if (email === "" || password === "") {
+      Alert.alert("Error", "Vui lòng nhập đầy đủ thông tin!");
+      return;
+    }
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => console.log("Đăng nhập thành công"))
+      .catch((err) => Alert.alert("Lỗi đăng nhập", "Tài khoản không tồn tại"));
+      // .catch((err) => Alert.alert("Lỗi đăng nhập", err.message));
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image source={backImage} style={styles.backImage} />
+      <View style={styles.whiteSheet} />
+      <SafeAreaView style={styles.form}>
+        <Image source={iconImage} style={styles.logo} />
+        <Text style={styles.title}>ĐĂNG NHẬP</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Nhập địa chỉ email"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+          autoFocus={true}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <View style={styles.passwordContainer}>
+          <TextInput
+            style={styles.passwordInput}
+            placeholder="Nhập mật khẩu"
+            autoCapitalize="none"
+            autoCorrect={false}
+            secureTextEntry={!showPassword}
+            textContentType="password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+          <TouchableOpacity
+            style={styles.eyeIcon}
+            onPress={() => setShowPassword(!showPassword)}
+          >
+            <Ionicons name={showPassword ? "eye-off" : "eye"} size={24} color="grey" />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.button} onPress={onHandleLogin}>
+          <Text style={{ fontWeight: 'bold', color: '#fff', fontSize: 18 }}>Đăng nhập</Text>
+        </TouchableOpacity>
+        <View style={{ marginTop: 20, flexDirection: 'row', alignItems: 'center', alignSelf: 'center' }}>
+          <Text style={{ color: 'gray', fontWeight: '600', fontSize: 14 }}>Bạn chưa có Tài khoản? </Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
+            <Text style={{ color: '#f57c00', fontWeight: '600', fontSize: 14 }}>Đăng ký</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+      <StatusBar barStyle="light-content" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: "orange",
+    alignSelf: "center",
+    paddingBottom: 24,
+  },
+  input: {
+    backgroundColor: "#F6F7FB",
+    height: 58,
+    marginBottom: 20,
+    fontSize: 16,
+    borderRadius: 10,
+    padding: 12,
+  },
+  passwordContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: "#F6F7FB",
+    height: 58,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  passwordInput: {
+    flex: 1,
+    fontSize: 16,
+    padding: 12,
+  },
+  eyeIcon: {
+    padding: 10,
+  },
+  backImage: {
+    width: "100%",
+    height: 340,
+    position: "absolute",
+    top: 0,
+    resizeMode: 'cover',
+  },
+  whiteSheet: {
+    width: '100%',
+    height: '75%',
+    position: "absolute",
+    bottom: 0,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 60,
+  },
+  form: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 30,
+  },
+  button: {
+    backgroundColor: '#f57c00',
+    height: 58,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  logo: {
+    width: 100, // Đặt chiều rộng tùy ý
+    height: 100, // Đặt chiều cao tùy ý
+    alignSelf: "center", // Đặt logo ở giữa theo chiều ngang
+    marginBottom: 20, // Khoảng cách bên dưới logo
+  },
+});
